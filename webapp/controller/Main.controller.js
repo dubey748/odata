285f0c935d147ad0a770baa14e14f0e30b11ad88
Abhishek Dubey <abhishek.du@icloud.com>
2025-07-13 20:03:22 +0000

    Added History log feature

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -11,6 +11,7 @@ sap.ui.define([
 
             var workItems = [];
             var statuses = [];
+            var historyLog = []
 
             if (dataModel.getProperty("/workItems")) {
                 workItems = dataModel.getProperty("/workItems");
@@ -18,6 +19,9 @@ sap.ui.define([
 
             if (dataModel.getProperty("/statuses")) {
                 statuses = dataModel.getProperty("/statuses");
+            }
+            if (dataModel.getProperty("/historyLog")) {
+                historyLog = dataModel.getProperty("/historyLog");
             }
 
             var initialModel = {
@@ -29,7 +33,8 @@ sap.ui.define([
                 },
                 workItems: workItems,
                 statuses: statuses,
-                currentEditIndex: -1
+                currentEditIndex: -1,
+                historyLog: historyLog
             };
 
             var oModel = new JSONModel(initialModel);
@@ -40,11 +45,15 @@ sap.ui.define([
             var newItem = oModel.getProperty("/newItem");
             var index = oModel.getProperty("/currentEditIndex");
             var workItems = oModel.getProperty("/workItems") || [];
+            var historyLog = oModel.getProperty("/historyLog") || []; // ✅ Declare historyLog properly
+
+            let action = "";
 
             if (index > -1) {
                 // 🔁 Update existing item
                 workItems[index] = Object.assign({}, newItem);
                 oModel.setProperty("/workItems", workItems);
+                action = "Updated";
 
                 // Reset state
                 oModel.setProperty("/currentEditIndex", -1);
@@ -53,7 +62,21 @@ sap.ui.define([
                 var itemToAdd = Object.assign({}, newItem);
                 workItems.push(itemToAdd);
                 oModel.setProperty("/workItems", workItems);
+                action = "Created";
             }
+
+            // Clone item for logging
+            var itemToLog = Object.assign({}, newItem);
+
+            // ✅ Push into historyLog correctly
+            historyLog.push({
+                action: action,
+                item: itemToLog,
+                timestamp: this._getFormattedTimestamp()
+            });
+
+            // ✅ Update historyLog back into the model
+            oModel.setProperty("/historyLog", historyLog);
 
             // Clear form
             oModel.setProperty("/newItem", {
@@ -66,12 +89,22 @@ sap.ui.define([
         onDelete: function (oEvent) {
             var oModel = this.getView().getModel();
             var workItems = oModel.getProperty("/workItems");
+            var historyLog = oModel.getProperty("/historyLog") || [];
+            // Get index from the clicked row
             var oContext = oEvent.getSource().getBindingContext();
             var index = oContext.getPath().split("/").pop(); // Get index
 
+            // Store the item before deleting
+            var deletedItem = Object.assign({}, workItems[index]);
             // Remove the item
             workItems.splice(index, 1);
             oModel.setProperty("/workItems", workItems);
+            historyLog.push({
+                action: "Deleted",
+                item: deletedItem,
+                timestamp: this._getFormattedTimestamp()
+            });
+            oModel.setProperty("/historyLog", historyLog);
         },
         onEdit: function (oEvent) {
             var oModel = this.getView().getModel();
@@ -85,6 +118,19 @@ sap.ui.define([
 
             // Store index to know which item to update
             oModel.setProperty("/currentEditIndex", index);
+        },
+        _getFormattedTimestamp: function () {
+            var now = new Date();
+            return now.toLocaleString();
+        },
+        formatHistoryEntry: function (action, item, timestamp) {
+            if (!item) return "";
+
+            const id = item.id || "N/A";
+            const assignee = item.name || "Unknown";
+            const status = item.status || "Unknown";
+
+            return `Task (ID: ${id}) was ${action} by "${assignee}"\nStatus: ${status} on ${timestamp}`;
         }
 
     });
